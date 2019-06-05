cc7f3ba0d3e20065bcb356d686a305a517590ce7
luandro <luandro@gmail.com>
2019-06-05 11:32:31 -0300

    added photos uploading

--- components/forms/Upload.js
+++ components/forms/Upload.js
@@ -1,71 +1,62 @@
-import { Mutation } from 'react-apollo'
-import UPLOAD_FILE from '../../queries/uploadFile.gql'
-import React from 'react'
-import PropTypes from 'prop-types'
-import { withStyles } from '@material-ui/core/styles'
-import Button from '@material-ui/core/Button'
+import { Mutation } from 'react-apollo';
+import UPLOAD_FILE from '../../queries/uploadFile.gql';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
-const styles = theme => ({
-  button: {
-    margin: theme.spacing(1),
-  },
-  input: {
-    display: 'none',
-  },
-})
+const styles = (theme) => ({
+	button: {
+		margin: theme.spacing(1)
+	},
+	input: {
+		display: 'none'
+	}
+});
 
-const Upload = ({
-  required,
-  multiple,
-  accept,
-  classes,
-  handleUpload,
-  meta,
-}) => (
-  <div>
-    <Mutation
-      mutation={UPLOAD_FILE}
-    >
-      {upload => (
-        <input
-          accept={accept || "*"}
-          multiple={multiple}
-          required={required}
-          type="file"
-          className={classes.input}
-          id="contained-button-file"
-          onChange={({
-            target: {
-              validity,
-              files: [file]
-            }
-          }) => {
-            validity.valid
-            upload({ variables: { file } })
-              .then(res => {
-                if (res && res.data.uploadFile) {
-                  const url = res.data.uploadFile.url
-                  handleUpload(url)
-                }
-              })
-          }}
-        />
-      )}
-    </Mutation>
-    <label htmlFor="contained-button-file">
-      <Button variant="contained" component="span" className={classes.button}>
-        Upload
-      </Button>
-      <span>{meta.touched ? meta.error : undefined}</span>
-      <span>{meta.error && meta.touched}</span>
-    </label>
-  </div>
-)
-
+const Upload = ({ required, multiple, accept, classes, handleUpload, meta }) => (
+	<div>
+		<Mutation mutation={UPLOAD_FILE}>
+			{(upload) => (
+				<input
+					accept={accept || '*'}
+					multiple={multiple}
+					required={required}
+					type="file"
+					className={classes.input}
+					id="contained-button-file"
+					onChange={({ target: { validity, files } }) => {
+						validity.valid;
+						let urlList = [];
+						Promise.all(
+							Object.keys(files).map(async (fileKey) => {
+								const file = files[fileKey];
+								await upload({ variables: { file } }).then((res) => {
+									if (res && res.data.uploadFile) {
+										const url = res.data.uploadFile.url;
+										urlList.push(url);
+									}
+								});
+							})
+						).then((i) => {
+							handleUpload(urlList);
+						});
+					}}
+				/>
+			)}
+		</Mutation>
+		<label htmlFor="contained-button-file">
+			<Button variant="contained" component="span" className={classes.button}>
+				Upload
+			</Button>
+			<span>{meta.touched ? meta.error : undefined}</span>
+			<span>{meta.error && meta.touched}</span>
+		</label>
+	</div>
+);
 
 Upload.propTypes = {
-  classes: PropTypes.object.isRequired,
-}
+	classes: PropTypes.object.isRequired
+};
 
-export default withStyles(styles)(Upload)
-
+export default withStyles(styles)(Upload);
--- components/forms/project.js
+++ components/forms/project.js
@@ -13,50 +13,6 @@ import OutlineTextField from './OutlineTextField';
 import Select from './Select';
 import MultiSelect from '../MultiSelect';
 
-const validInputList = {
-	name: {
-		label: 'Nome do projeto',
-		type: 'text'
-	},
-	key: {
-		label: 'Slug',
-		type: 'text',
-		required: true
-	},
-	description: {
-		label: 'Descrição do projeto',
-		type: 'html'
-	}
-	// media: String
-	// photos: [String]
-	// categoryId: String
-	// tags: [String]
-	// startCall: {
-	// 	label: 'Início da chamada',
-	// 	type: 'date',
-	// 	required: true
-	// },
-};
-
-function isValidDate(dateString) {
-	var regex_date = /^\d{4}\-\d{1,2}\-\d{1,2}$/;
-	if (!regex_date.test(dateString)) {
-		return false;
-	}
-	var parts = dateString.split('-');
-	var day = parseInt(parts[2], 10);
-	var month = parseInt(parts[1], 10);
-	var year = parseInt(parts[0], 10);
-	if (year < 1000 || year > 3000 || month == 0 || month > 12) {
-		return false;
-	}
-	var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
-	if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)) {
-		monthLength[1] = 29;
-	}
-	return day > 0 && day <= monthLength[month - 1];
-}
-
 const validate = (values) => {
 	const errors = {};
 	// if (!values.slug) {
@@ -95,6 +51,12 @@ const styles = (theme) => ({
 		flexBasis: '33.33%',
 		width: '100%'
 	},
+	inputs: {
+		minWidth: 300,
+		display: 'flex',
+		flexFlow: 'column',
+		alignItems: 'center'
+	},
 	selectors: {
 		height: 400,
 		display: 'flex',
@@ -104,159 +66,141 @@ const styles = (theme) => ({
 	}
 });
 
-class IssueForm extends Component {
-	state = {
-		uploaded: null
-	};
+const ProjectForm = ({ classes, onSubmit, project, publish }) => {
+	const [ uploadedList, setUpload ] = React.useState([]);
 
-	clearUpload = () => this.setState({ uploaded: null });
+	const clearUpload = () => setUpload([]);
 
-	handleUpload = (uploaded, change, blur) => {
+	const handleUpload = (uploaded, change, blur) => {
+		console.log('uploaded', uploaded);
 		blur('image');
 		change('image', uploaded);
-		this.setState({ uploaded });
+		setUpload(uploadedList.concat(uploaded));
 	};
 
-	onEditorStateChange = (editor, change, blur) => {
+	const onEditorStateChange = (editor, change, blur) => {
 		blur('body');
 		change('body', editor);
 	};
-
-	render() {
-		const { uploaded } = this.state;
-		const { classes, onSubmit, project, publish } = this.props;
-
-		return (
-			<Form
-				initialValues={project ? formatedIssue : {}}
-				onSubmit={async (e) => {
-					let cleanList = {};
-					Object.keys(validInputList).map((valid) => {
-						Object.keys(e).map((i) => {
-							if (i === valid && e[i] !== null) {
-								if (isValidDate(e[i])) {
-									/* parse date */
-									cleanList[i] = new Date(e[i]).toISOString();
-								} else if (0 === e[i] % (!isNaN(parseFloat(e[i])) && 0 <= ~~e[i])) {
-									/* parse ints */
-									cleanList[i] = parseInt(e[i]);
-								} else {
-									cleanList[i] = e[i];
-								}
-							}
-						});
-					});
-					await onSubmit(cleanList);
-					this.clearUpload();
-				}}
-				validate={validate}
-				render={({ handleSubmit, pristine, invalid, form: { change, blur } }) => (
-					<form onSubmit={handleSubmit}>
-						<Paper className={classes.root} elevation={1}>
-							<span>{project && project.key}</span>
-							{/* <div className={classes.column}>
-								<Typography component="h4" variant="h4">
-									Capa
-								</Typography>
-							</div>
-							<div className={classes.column}>
-								{uploaded && <img src={uploaded} />}
-								{!uploaded && project && project.image && <img src={project.image} />}
-								{!uploaded && !project && <h4>Sem imagem de capa</h4>}
-							</div>
-							<div className={classNames(classes.column, classes.helper)}>
-								<Typography variant="caption">
-									Imagem de capa do projeto
-									<br />
-									<Field name="image">
-										{(fieldprops) => (
-											<Upload
-												{...fieldprops}
-												accept="image/*"
-												handleUpload={(url) => this.handleUpload(url, change, blur)}
-											/>
-										)}
-									</Field>
-								</Typography>
-							</div> */}
-							<Typography component="h1" variant="h3" style={{ padding: '35px 0' }}>
-								Novo projeto
+	return (
+		<Form
+			initialValues={project ? formatedIssue : {}}
+			onSubmit={async (e) => {
+				let cleanList = {};
+				await onSubmit(cleanList);
+				clearUpload();
+			}}
+			validate={validate}
+			render={({ handleSubmit, pristine, invalid, form: { change, blur } }) => (
+				<form onSubmit={handleSubmit}>
+					<Paper className={classes.root} elevation={1}>
+						<span>{project && project.key}</span>
+						<Typography component="h1" variant="h3" style={{ padding: '35px 0' }}>
+							Novo projeto
+						</Typography>
+						<Divider />
+						<div className={classes.inputs}>
+							<Typography component="h5" variant="h5">
+								Nome do projeto
 							</Typography>
-							<Divider />
-							{Object.keys(validInputList)
-								.filter((k) => {
-									if (validInputList[k].type !== 'file') {
-										return validInputList[k];
-									}
-								})
-								.map((input) => (
-									<div className={classes.full} key={input}>
-										{validInputList[input].type === 'html' && (
-											<h3>{validInputList[input].label}</h3>
-										)}
-										<Field
-											name={input}
-											component={OutlineTextField}
-											inputType={validInputList[input].type}
-											label={validInputList[input].label}
-											required={validInputList[input].required || false}
-											onEditorStateChange={
-												validInputList[input].type === 'html' ? (
-													(e) => this.onEditorStateChange(e, change, blur)
-												) : null
-											}
-										/>
-									</div>
-								))}
-							<div className={classes.selectors}>
-								<Typography component="h5" variant="h5">
-									Categoria
-								</Typography>
-								<div className={classes.column}>
-									<Select label={'Selecione a categoria deste projeto'} />
-									<Button size="small" color="primary">
-										Nova categoria
-									</Button>
-								</div>
-								<Typography component="h5" variant="h5">
-									Etiquetas
-								</Typography>
-								<div className={classes.column}>
-									<MultiSelect label={'Selectione as etiquetas deste projeto'} />
-									<Button size="small" color="primary">
-										Nova etiqueta
-									</Button>
-								</div>
+							<Field name={'name'} component={OutlineTextField} inputType={'text'} />
+							<Typography component="h5" variant="h5">
+								Descrição do projeto
+							</Typography>
+							<Field
+								name={'description'}
+								component={OutlineTextField}
+								inputType={'html'}
+								onEditorStateChange={(e) => onEditorStateChange(e, change, blur)}
+							/>
+						</div>
+						<div className={classes.selectors}>
+							<Typography component="h5" variant="h5">
+								Categoria
+							</Typography>
+							<div className={classes.column}>
+								<Select label={'Selecione a categoria deste projeto'} />
+								<Button size="small" color="primary">
+									Nova categoria
+								</Button>
+								<Button size="small" color="primary">
+									Atualizar categoria
+								</Button>
 							</div>
-							<Divider />
-							<Button size="small">Cancel</Button>
-							<Button size="small" color="primary" type="submit" disabled={pristine || invalid}>
-								Salvar
-							</Button>
-							<Button
-								size="small"
-								color="default"
-								onClick={() => publish({ variables: { projectId: project.id } })}
-								disabled={!project || !project.publishedCall || project.published}
-							>
-								Publicar projeto
-							</Button>
-						</Paper>
-						<style jsx>{`
-							img {
-								max-width: 300px;
-								max-height: 300px;
-							}
-						`}</style>
-					</form>
-				)}
-			/>
-		);
-	}
-}
+							<Typography component="h5" variant="h5">
+								Etiquetas
+							</Typography>
+							<div className={classes.column}>
+								<MultiSelect label={'Selectione as etiquetas deste projeto'} />
+								<Button size="small" color="primary">
+									Nova etiqueta
+								</Button>
+								<Button size="small" color="primary">
+									Atualizar etiquetas
+								</Button>
+							</div>
+						</div>
+						<div className={classes.column}>
+							<Typography component="h4" variant="h4">
+								Fotos
+							</Typography>
+						</div>
+						<div className={classes.column}>
+							{uploadedList.length > 0 &&
+								uploadedList.map((image) => (
+									<img
+										key={image}
+										src={image}
+										style={{ maxWidth: `${100 / uploadedList.length}%` }}
+									/>
+								))}
+							{/* {!uploaded && project && project.image && <img src={project.image} />} */}
+							{uploadedList.length === 0 && !project && <h4>Este projeto não tem imagens</h4>}
+						</div>
+						<div className={classNames(classes.column, classes.helper)}>
+							<Typography variant="caption">
+								Fotos do projeto
+								<br />
+								<Field name="image">
+									{(fieldprops) => (
+										<Upload
+											multiple={true}
+											{...fieldprops}
+											accept="image/*"
+											handleUpload={(url) => handleUpload(url, change, blur)}
+										/>
+									)}
+								</Field>
+							</Typography>
+						</div>
+						<Divider />
+						<Button size="small">Cancel</Button>
+						<Button size="small" color="primary" type="submit" disabled={pristine || invalid}>
+							Salvar
+						</Button>
+						<Button
+							size="small"
+							color="default"
+							onClick={() => publish({ variables: { projectId: project.id } })}
+							disabled={!project || !project.publishedCall || project.published}
+						>
+							Publicar projeto
+						</Button>
+					</Paper>
+					<style jsx>{`
+						img {
+							max-width: 300px;
+							max-height: 300px;
+						}
+					`}</style>
+				</form>
+			)}
+		/>
+	);
+};
 
-IssueForm.propTypes = {
+ProjectForm.propTypes = {
 	classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(IssueForm);
+export default withStyles(styles)(ProjectForm);
