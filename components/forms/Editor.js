bb34e4ea76cc6011379508552461e5d97fff1c7c
luandro <luandro@gmail.com>
2019-08-05 20:23:54 -0300

    added video linking to rich text editor

--- components/forms/Editor.js
+++ components/forms/Editor.js
@@ -1,12 +1,51 @@
-import dynamic from 'next/dynamic';
-const ReactQuill = dynamic(() => import('react-quill'), {
-	ssr: false
-});
+import dynamic from "next/dynamic"
+
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" }
+    ],
+    ["link", "image", "video"],
+    ["clean"]
+  ]
+}
+
+const formats = [
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+  "video"
+]
+
+const ReactQuill = dynamic(() => import("react-quill"), {
+  ssr: false
+})
 
 export default ({ onEditorStateChange, value }) => {
-	if (window) {
-		return <ReactQuill onChange={(e) => onEditorStateChange(e)} defaultValue={value} value={value} />;
-	} else {
-		return <input type="textarea" />;
-	}
-};
+  if (window) {
+    return (
+      <ReactQuill
+        onChange={e => onEditorStateChange(e)}
+        defaultValue={value}
+        value={value}
+        modules={modules}
+        formats={formats}
+      />
+    )
+  } else {
+    return <input type='textarea' />
+  }
+}
